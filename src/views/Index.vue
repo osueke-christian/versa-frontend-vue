0fe7c3c45d60695b9d84d23324523c2c944c57b0
Christian Osueke <christian.osueke@vfdgroup.com>
2020-02-07 20:22:06 +0100

    Added header section

---
+++ src/views/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <header-section/>
+</template>
+
+<script>
+
+import Header from './components/Header.vue'
+
+export default {
+    name: 'Home',
+
+    data () {
+      return {
+        
+      }
+    },
+
+    components: {
+      'header-section': Header
+    },
+}
+</script>
